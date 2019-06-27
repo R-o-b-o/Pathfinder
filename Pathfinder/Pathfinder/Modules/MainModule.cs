0d2361624492ae377d1c3ba8b441991ddb1e0a74
R-o-b-o <39059788+R-o-b-o@users.noreply.github.com>
2019-06-27 11:41:15 +0100

    added command framework and ping command

---
+++ Pathfinder/Pathfinder/Modules/MainModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Discord;
+using Discord.Commands;
+using Pathfinder.Services;
+
+namespace Pathfinder.Modules
+{
+    public class MainModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        [Alias("pong")]
+        public async Task PingAsync()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var message = await ReplyAsync(".");
+            watch.Stop();
+            await message.ModifyAsync(msg => msg.Content = string.Format("that took **{0}ms**", watch.ElapsedMilliseconds));
+        }
+
+    }
+}
--- Pathfinder/Pathfinder/Program.cs
+++ Pathfinder/Pathfinder/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
+using System.Net.Http;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Pathfinder.Services;
+using DotNetEnv;
 
 namespace Pathfinder
 {
     class Program
     {
         static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
+            DotNetEnv.Env.Load();
+
+            var services = ConfigureServices();
+            var client = services.GetRequiredService<DiscordSocketClient>();
+
+            // Tokens should be considered secret data and never hard-coded.
+            // We can read from the environment variable to avoid hardcoding.
+            await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("token"));
+            await client.StartAsync();
+
+            // Here we initialize the logic required to register our commands.
+            await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private IServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandlingService>()
+                .AddSingleton<HttpClient>()
+                .BuildServiceProvider();
         }
     }
 }
